775907f7424501edd7cdb53b9a6b45fb409b18c8
DaviF <davifiquer@gmail.com>
2022-10-06 19:03:21 -0300

    First commit

---
+++ spring-estudo/src/test/java/br/com/fiquer/springestudo/SpringEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiquer.springestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
